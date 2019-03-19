cdeae36197382c160809d52388406c3592312487
Raunak Dutta <45163382+Raunak-Dutta@users.noreply.github.com>
2019-03-19 20:46:21 +0800

    Initial commit

---
+++ src/Prac_1_1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Prac_1_1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int j = sc.nextInt();
+        for(int i = 0; i < j; i++){
+            System.out.println(i);
+        }
+    }
+}
